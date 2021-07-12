47a04ef0807b305d5a607a7cc5af37f8c148fc08
Peter Keenan <peter.keenan@sage.com>
2021-07-12 14:58:32 +0100

    Add BooksController and two GET endpoints

---
+++ NETCoreAPI/Controllers/BooksController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCoreAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookContext _context;
+
+        public BooksController(BookContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Book>>> GetBooksAsync()
+        {
+            return await _context.Books.ToListAsync();
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Book>> GetBookByIdAsync(int id)
+        {
+            var book = await _context.Books.FirstOrDefaultAsync(b => b.BookId == id);
+
+            if (book == null) return NotFound();
+
+            return book;
+        }
+    }
+}
